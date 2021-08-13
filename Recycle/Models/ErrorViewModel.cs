935d8d794a9aeb298dbd1e3d22028fe5bfb89aef
Yotam Levy <yotamlevy12@cs.colman.ac.il>
2021-08-13 09:02:05 +0300

    Initial commit

---
+++ Recycle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Recycle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
