122f086c0fcd57fe8206d424b29eb39e87906922
jonathan_meiri <yonn200@gmail.com>
2021-08-19 17:09:40 +0300

    user gender added

--- Recycle/Models/User.cs
+++ Recycle/Models/User.cs
@@ -11,21 +11,29 @@ namespace Recycle.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name is required.")]
         public string FirstName { get; set; }
+
         [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Last Name is required.")]
         public string LastName { get; set; }
 
         [EmailAddress]
+        [Required(ErrorMessage = "Email address is required.")]
         public string Email { get; set; }
 
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password address is required.")]
         public string Password { get; set; }
 
-        public string Gender { get; set; }
+        //[Required(ErrorMessage = "Gender is required.")]
+        public UserGender Gender { get; set; }
 
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Birth Date is required.")]
         public DateTime BDay { get; set; }
 
         public double Balance { get; set; } = 0.0;
