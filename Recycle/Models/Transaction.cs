1ce6ee440bcdc3c9e4e7307f55235dbfbf7fb742
Eden1996 <88877430+Eden1996@users.noreply.github.com>
2021-08-24 11:01:15 +0300

    Transaction.cs
    TEST

---
+++ Recycle/Models/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recycle.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int FromUserID { get; set; }
+
+        public int ToUserID { get; set; }
+
+        public int Amount { get; set; }
+
+        public int CartID { get; set; }
+
+        public int NumTransaction { get; set; }
+
+//starus(pending, approve, dicline)
+
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
+
+
+
+
